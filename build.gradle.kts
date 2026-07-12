85f3a4224e8c0df24ef87ead2fed9d1385a5a098
Frostre_ <n9043395@gmail.com>
2026-07-12 17:22:10 +0200

    Add plugins for Android and Kotlin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,6 @@
-
+// Top-level build file for materialistic
+plugins {
+    alias("com.android.application") version "8.5.0" apply false
+    alias("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    alias("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+}
