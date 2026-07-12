084a6d3057e27665fbd589c880bba53b5d71d9e7
Frostre_ <n9043395@gmail.com>
2026-07-12 19:55:07 +0200

    Import dp for unit measurements in MainActivity

--- app/src/main/java/com/frostre1997/materialistic/MainActivity.kt
+++ app/src/main/java/com/frostre1997/materialistic/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.Modifier.Companion.fillMaxSize
+import androidx.compose.ui.unit.dp
 import com.frostre1997.materialistic.ui.MaterialisticTheme
 
 class MainActivity : ComponentActivity() {
