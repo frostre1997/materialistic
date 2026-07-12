aab7b203b7738c3a257050db60c54d0f54fb2a1a
Frostre_ <n9043395@gmail.com>
2026-07-12 19:25:50 +0200

    Bump version name to 1.0.1-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0-alpha"
+        versionName = "1.0.1-alpha"
 
         vectorDrawables.useSupportLibrary = true
     }
