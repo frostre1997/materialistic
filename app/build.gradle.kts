53c40859b0130fb30ca04edae7c6ff91029193e0
Frostre_ <n9043395@gmail.com>
2026-07-12 18:05:19 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias("com.android.application")
-    alias("org.jetbrains.kotlin.android")
-    alias("org.jetbrains.kotlin.plugin.compose")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
